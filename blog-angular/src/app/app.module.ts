86366a4a53b13f3bda219ffa71efae4d7440085a
Yago Castro Gomes <106767926+yago-castro-gomes@users.noreply.github.com>
2023-10-21 09:13:11 -0300

    cria menu-title

---
+++ blog-angular/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MenuTitleComponent } from './menu-title/menu-title.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuTitleComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
