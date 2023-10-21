ee683bf8bebd0f11852a1c5b0159d8f508e95a6e
Yago Castro Gomes <106767926+yago-castro-gomes@users.noreply.github.com>
2023-10-21 09:38:22 -0300

    entrega blog estuturado

--- blog-angular/src/app/app.module.ts
+++ blog-angular/src/app/app.module.ts
@@ -4,11 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuTitleComponent } from './menu-title/menu-title.component';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuTitleComponent
+    MenuTitleComponent,
+    MenuBarComponent,
+    MenuBarComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
